23875a3772aa561aa5328d481ad89b276f096767
Tamas Zentai <zentaitamas@gmail.com>
2021-06-20 14:47:34 +0100

    add location based call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { monitorEventLoopDelay } from 'perf_hooks';
 import React, {useState, useEffect} from 'react';
 import './App.css';
-
-import axios from 'axios';
 
 import InputField from './components/inputField';
 
@@ -10,15 +7,30 @@ function App() {
   const [cityName, setCityName] = useState('');
   const [data, setData] = useState();
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const locationBasedCall = async () => {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_APIKEY}`)
+        const data = await response.json();
+        setData(data);
+        console.log(data);
+      }
+      locationBasedCall();
+    console.log("Latitude is :", position.coords.latitude);
+    console.log("Longitude is :", position.coords.longitude);
+    });
+  },[])
+
   const cityNameOnChangeHandler = (event: any) => {
     setCityName(event.target.value);
   }
 
   const onSubmitHandler = async (event: any) => {
     event.preventDefault();
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_APIKEY}`)
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
     const data = await response.json()
     console.log(data)
+    setData(data);
   }
 
   return (
